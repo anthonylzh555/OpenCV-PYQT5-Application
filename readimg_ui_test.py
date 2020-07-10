e6e14f04395b9a7abc0ba76179bd7c87df7ec14b
anthonylzh555 <anthonylzh555@gmail.com>
2020-07-10 11:47:11 +0800

    Add Function
    - SaveSlot
    - processSlot

--- readimg_ui_test.py
+++ readimg_ui_test.py
@@ -6,7 +6,8 @@ from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog, QGridLayout, QLabel, QPushButton
 
 
-class win(QDialog):
+class mainUI(QDialog):
+    
     def __init__(self):
         """
         imitialize a numpy array to save  a photo
@@ -19,7 +20,6 @@ class win(QDialog):
         """
         deifine the user interface
         """
-
         # Define Size
         self.resize(400, 300)
 
@@ -66,18 +66,54 @@ class win(QDialog):
         self.refreshShow()
 
     def refreshShow(self):
-        # Transform OpenCV image into Qimage
-        height, width, channel = self.img.shape
-        bytesPerline = 3 * width
+        """
+        Transform OpenCV image into Qimage
+        Then Refresh
+        """
+        if len(self.img.shape) == 3 :
+            print("test")
+            height, width, channel = self.img.shape
+            bytesPerline = 3 * width
+        
+        elif  len(self.img.shape) == 2 :
+            print("test2")
+            height, width = self.img.shape
+            bytesPerline = 1 * width
+
+
+        # Qimage read image
         self.qImg = QImage(self.img.data, width, height, bytesPerline, QImage.Format_RGB888).rgbSwapped()
         
         # show Qimage
         self.label.setPixmap(QPixmap.fromImage(self.qImg))
+        
+    def saveSlot(self):
+        """
+        Save Photo
+        """
+        fileName, tmp = QFileDialog.getSaveFileName(self, 'Save Image', 'Image', '*.png *.jpg *.bmp')
+        if fileName is '':
+            return
+        if self.img.size == 1:
+            return
 
+        # Calling OpenCV function to save photo
+        cv2.imwrite(fileName, self.img)
 
+    def processSlot(self):
+        """
+        Process Data Here
+        """
+        if self.img.size == 1:
+            return
+        
+        #Processing Image
+        self.img = cv2.blur(self.img, (5, 5))
+        #self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        self.refreshShow()
 
 if __name__ == '__main__':
-    a = QApplication(sys.argv)
-    w = win()
-    w.show()
-    sys.exit(a.exec_())
+    app = QApplication(sys.argv)
+    mainwindow = mainUI()
+    mainwindow.show()
+    sys.exit(app.exec_())
